316acb8dca15e7a1504a7de38e6bc7beef04754a
Ray Andrew <raydreww@gmail.com>
2019-04-27 17:16:36 +0700

    modify create input

--- utils/dataset.py
+++ utils/dataset.py
@@ -300,18 +300,17 @@ def create_input_files(dataset,
                 tags_per_image = old_tags_per_image
 
                 # Read images
-
-                # img = imread(impaths[i])
-                # if len(img.shape) == 2:
-                #     img = img[:, :, np.newaxis]
-                #     img = np.concatenate([img, img, img], axis=2)
-                # img = imresize(img, (256, 256))
-                # img = img.transpose(2, 0, 1)
-                # assert img.shape == (3, 256, 256)
-                # assert np.max(img) <= 255
+                img = imread(impaths[i])
+                if len(img.shape) == 2:
+                    img = img[:, :, np.newaxis]
+                    img = np.concatenate([img, img, img], axis=2)
+                img = imresize(img, (256, 256))
+                img = img.transpose(2, 0, 1)
+                assert img.shape == (3, 256, 256)
+                assert np.max(img) <= 255
 
                 # Save image to HDF5 file
-                # images[i] = img
+                images[i] = img
 
                 for j, c in enumerate(captions):
                     # Encode captions
