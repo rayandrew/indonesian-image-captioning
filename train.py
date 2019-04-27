96a6894c610fa0359f3c6dcc018fe01f3a5c6fea
Ray Andrew <raydreww@gmail.com>
2019-04-27 18:06:00 +0700

    fix syntax error

--- train.py
+++ train.py
@@ -95,7 +95,7 @@ def main():
     # Custom dataloaders
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
-    train_loader = torch.utils.data.DataLoader(data_folder
+    train_loader = torch.utils.data.DataLoader(
         CaptionDataset(data_folder, data_name, 'TRAIN', transform=transforms.Compose([normalize])),
         batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
     val_loader = torch.utils.data.DataLoader(
