2b2fd5e4630a77670ba478699734d8ac397ff0df
Ray Andrew <raydreww@gmail.com>
2019-05-02 00:35:22 +0700

    [NEPTUNE] remove env

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='[(S)how (A)ttend (T)ell - (S)emantic (C)ompositional (N)etworks] - Train Script')
 
+    parser.add_argument('--neptune_key', '-nk', help='neptune key')
     parser.add_argument('--neptune_user', '-nu',
                         default='rayandrew', help='neptune username')
     parser.add_argument('--type', '-t', help='train model type')
--- trains/pure_attention.py
+++ trains/pure_attention.py
@@ -70,7 +70,8 @@ def main(args):
 
     print('Initializing neptune-ml')
 
-    neptune.init(project_qualified_name=args.neptune_user + '/' + args.type)
+    neptune.init(api_token=args.neptune_key,
+                 project_qualified_name=args.neptune_user + '/' + args.type)
 
     experiment = neptune.create_experiment(params={
         'epochs': epochs,
